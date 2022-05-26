eb86e35bfe5141f1386d2943c9342831437864de
Wilbert Reed III <reediiiw@Wilberts-MacBook-Pro.local>
2022-05-26 18:18:45 -0400

    success in mapping over data in api

--- weatherapp/src/components/Card/Card.jsx
+++ weatherapp/src/components/Card/Card.jsx
@@ -26,45 +26,47 @@ export default function Card() {
         try{
             const res = await fetch(URL);
             const json = await res.json();
-            setData(json);
+            setData(json.list);
+            console.log(json.list);
         }catch(e) {
             console.log(e)
         }
     };
-    
+
+
+
     
     useEffect(()=> {
-        console.log(weatherData());
+        (weatherData(URL));
     }, [])
 
     
 	return (
-        <div> 
-        {/* {data.map((props)=>{ 
-            const iconURL = `http://openweathermap.org/img/wn/ + ${props.list.weather.icon} + .png/`
+        <> 
+        {data.map((data)=>{ 
+        const iconURL = `http://openweathermap.org/img/wn/ + ${data.weather.icon} + .png`
             return (
-                    <div className="card-container">
-                        <div className="day-of-the-week">
-                            <h4>Monday</h4>
-                        </div>
-                        <div className="date">
-                            <h5>March 1st, 1:00pm</h5>
-                        </div>
-                        <div className="temp-img-container">
-                            <div className="img-container">
-                                <img  className="img" alt= 'sunny day' src={iconURL}></img> 
-                            </div>
-                        
-                            <div className="temp">
-                                <h3>{Math.floor((props.list.main.temp - 273.15)*1.8000 +32.00)}</h3>
-                            </div>
-                        </div>
-                        <div>
-                            <h6>{props.list.weather.description}</h6>
-                        </div>
-                    </div>
-                    )})}  */}
-        </div>
-
+		<div className="card-container">
+			<div className="day-of-the-week">
+				<h4>Monday</h4>
+			</div>
+			<div className="date">
+                <h5>{data.dt_txt}</h5>
+            </div>
+            <div className="temp-img-container">
+                <div className="img-container">
+                    <img  className="img" alt= 'sunny day' src={iconURL}></img>
+                </div>
+            
+                <div className="temp">
+                    <h3>{Math.floor((data.main.temp - 273.15)*1.8000 +32.00)}</h3>
+                </div>
+            </div>
+            <div>
+                <h6>{data.weather.description}</h6>
+            </div>
+		</div>
+        )})} 
+        </>
 	);
 }
