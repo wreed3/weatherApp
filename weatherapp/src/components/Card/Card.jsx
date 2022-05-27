60babf68a3056c0cbc33f4d4d17dffee0e49d926
Wilbert Reed III <reediiiw@Wilberts-MacBook-Pro.local>
2022-05-27 10:42:44 -0400

    update api

--- weatherapp/src/components/Card/Card.jsx
+++ weatherapp/src/components/Card/Card.jsx
@@ -1,65 +1,59 @@
 import React, {useState, useEffect} from "react";
 
 export default function Card() {
-
-    // const Detroit = {
-    //     lati: 42.331429,
-    //     longa: -83.045753,
-    //     APIKEY: "be306616b6cad0ecd57ce6ffa4d8844a"
-    // }
-
-    // const searchParams = new URLSearchParams(Detroit);
-
-    // const subString = searchParams.toString();
-
-    const lati = 42.331429;
-    const longa = -83.045753;
-
-    const APIKEY = "be306616b6cad0ecd57ce6ffa4d8844a";
-    const URL = `https://api.openweathermap.org/data/2.5/forecast?lat=${lati}&lon=${longa}&appid=${APIKEY}`;
-
-    const [data, setData] = useState('');
+    const [data, setData] = useState([]);
     
+    const options = {
+        method: 'GET',
+        headers: {
+            'X-RapidAPI-Host': 'openweather43.p.rapidapi.com',
+            'X-RapidAPI-Key': 'e9adca3a6cmsh3c85d74805897ecp1dc463jsnfe84a8f09fbf'
+        }
+    };
     
-    
+   
     const  weatherData = async () => {
         try{
-            const res = await fetch(URL);
+            const res = await fetch('https://openweather43.p.rapidapi.com/forecast?appid=da0f9c8d90bde7e619c3ec47766a42f4&q=Detroit&cnt=5&units=imperial', options);
             const json = await res.json();
             setData(json.list);
-            console.log(json.list);
+            console.log(json.list)
         }catch(e) {
             console.log(e)
         }
     };
-
-
-
+    
+    // const newData = [...data]
+    
     
     useEffect(()=> {
-        (weatherData(URL));
+        weatherData(URL);
+        //eslint-disable-next-line 
     }, [])
-
+    
     
 	return (
-        <> 
+        <React.Fragment> 
+            
+
         {data.map((data)=>{ 
-        const iconURL = `http://openweathermap.org/img/wn/ + ${data.weather.icon} + .png`
+            
+            const dayOfWeekName = new Date(data.dt_txt).toLocaleString('default', {weekday: 'long'});
             return (
 		<div className="card-container">
 			<div className="day-of-the-week">
-				<h4>Monday</h4>
+				<h4>{dayOfWeekName}</h4>
 			</div>
 			<div className="date">
                 <h5>{data.dt_txt}</h5>
             </div>
             <div className="temp-img-container">
                 <div className="img-container">
-                    <img  className="img" alt= 'sunny day' src={iconURL}></img>
+                    <img  className="img" alt= '' src={`http://openweathermap.org/img/wn/${data.weather.icon}.png`}></img>
                 </div>
             
                 <div className="temp">
-                    <h3>{Math.floor((data.main.temp - 273.15)*1.8000 +32.00)}</h3>
+                    <h3>{Math.floor((data.main.temp - 273.15)*1.8000 + 32.00)}</h3>
                 </div>
             </div>
             <div>
@@ -67,6 +61,6 @@ export default function Card() {
             </div>
 		</div>
         )})} 
-        </>
+        </React.Fragment>
 	);
 }
