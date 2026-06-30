2fcb47a8a4470ec81fe46fb3c11dc9410f9d4697
wreed3 <91444925+wreed3@users.noreply.github.com>
2026-06-30 01:19:35 -0400

    initialize weather app with express server and routes
    Files changed:
      - package.json (create)
      - server/.env (create)
      - server/routes/index.js (create)
      - server/routes/weather.js (create)
      - server/views/layout.jade (create)
      - server/views/index.jade (create)
      - server/views/weather.jade (create)
      - server/public/stylesheets/style.css (create)
      - server/app.js (update)

--- server/app.js
+++ server/app.js
@@ -3,11 +3,8 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-// Load environment variables
-require('dotenv').config();
-
-var routes = require('./routes/index');
-var weatherRoutes = require('./routes/weather');
+var indexRouter = require('./routes/index');
+var weatherRouter = require('./routes/weather');
 
 var app = express();
 
@@ -15,15 +12,14 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-// middleware
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-// routes
-app.use('/', routes);
-app.use('/api/weather', weatherRoutes);
+// Routes
+app.use('/', indexRouter);
+app.use('/weather', weatherRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -32,28 +28,15 @@ app.use(function(req, res, next) {
   next(err);
 });
 
-// error handlers
-
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
-    });
-  });
-}
-
-// production error handler
-// no stacktraces leaked to user
+// error handler
 app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
   res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
+  res.send('Error: ' + err.message);
 });
 
 module.exports = app;
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,19 +1,11 @@
 var express = require('express');
 var router = express.Router();
-var config = require('../config/weather-config');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Weather App' });
-});
-
-/* GET radar page */
-router.get('/radar', function(req, res, next) {
-  res.render('radar', { 
-    title: 'Weather Radar',
-    defaultLat: config.defaultLocation.lat,
-    defaultLon: config.defaultLocation.lon,
-    defaultZoom: config.defaultLocation.zoom
+  res.render('index', { 
+    title: 'Weather App',
+    message: 'Welcome to the Weather Application'
   });
 });
 
